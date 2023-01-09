cf737e63ba7e817e86e28ab69b1b0f0e03083b9c
Sartain, Alex <alex.sartain@zuhlke.com>
2023-01-09 10:50:27 +0000

    Add initial spring boot

---
+++ src/main/java/com/alsa/demo/LeaguesApplication.java
@@ -0,0 +1,13 @@
+package com.alsa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaguesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alsa/demo/LeaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alsa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
