3e2dd3c39f3977dbb73e0a2184ec360b42403383
Sartain, Alex <alex.sartain@zuhlke.com>
2023-01-10 11:47:22 +0000

    Add test for converting team name into id

--- src/main/java/com/alsa/demo/logic/ResultLogic.java
+++ src/main/java/com/alsa/demo/logic/ResultLogic.java
@@ -1,8 +1,11 @@
 package com.alsa.demo.logic;
 
 import com.alsa.demo.entities.Result;
+import com.alsa.demo.entities.Team;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResultLogic {
 
@@ -28,6 +31,14 @@ public class ResultLogic {
         String homeTeamAndResult = score.split("-")[1];
         String homeGoals = homeTeamAndResult.split("\\D+")[0];
         return Integer.parseInt(homeGoals.strip());
+    }
+
+    public static int givenInputReturnTeamIdMock(String teamName, List<Team> teams) {
+        List<Integer> teamIdsWithName = teams.stream().filter(e -> e.getName().equals(teamName)).map(Team::getId).toList();
+        if(teamIdsWithName.size() > 0)
+            return teamIdsWithName.get(0);
+        else
+            return -1;
     }
 
 }
--- src/test/java/com/alsa/demo/logic/ResultLogicTests.java
+++ src/test/java/com/alsa/demo/logic/ResultLogicTests.java
@@ -1,7 +1,11 @@
 package com.alsa.demo.logic;
 
 import com.alsa.demo.entities.Result;
+import com.alsa.demo.entities.Team;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,6 +15,15 @@ public class ResultLogicTests {
     private final String draw = "Tottenham 1-1 Arsenal";
     private final String multiName = "Manchester United 2-1 Manchester City Women";
     private final String multiScore = "Vision Express 20-20 Specsavers";
+    private List<Team> teams = Arrays.asList(new Team(1, "Everton"),
+            new Team(2, "Manchester City Women"),
+            new Team(3, "Liverpool"),
+            new Team(4, "Tottenham"),
+            new Team(5, "Manchester United"),
+            new Team(6, "Arsenal"),
+            new Team(7, "Vision Express"),
+            new Team(8, "Specsavers")
+    );
 
 
     @Test
@@ -55,6 +68,14 @@ public class ResultLogicTests {
     void getMultiGoalsAwayTeam() {
         int actualGoals = ResultLogic.givenInputReturnAwayGoals(multiScore);
         assertEquals(20, actualGoals);
+    }
+
+    @Test
+    void getTeamIdFromName() {
+        String teamName = ResultLogic.givenInputReturnAwayTeam(multiName);
+        assertEquals("Manchester City Women", teamName);
+        int teamId = ResultLogic.givenInputReturnTeamIdMock(teamName, teams);
+        assertEquals(2, teamId);
     }
 
 }
